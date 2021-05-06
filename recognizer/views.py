4474df55a3c7cd8591818e2e6c2443e76dc697e5
parthishere <parthishere1234@gmail.com>
2021-05-06 22:24:06 +0530

    Signup view and LoginView

--- recognizer/views.py
+++ recognizer/views.py
@@ -1,18 +1,79 @@
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import UserProfile, User
+from .forms import UserProfileForm, AuthenticationForm
+
+from django.contrib.auth import (
+    login,
+    authenticate,
+    get_user_model,
+    logout
+)
+
 
 # Create your views here.
 def home_view(request):
-    return HttpResponse("hello")
+    context = {}
+    context['data'] = 'some data'
+    return render(request, 'recognizer/home.html', context={})
 
 
 def login_view(request):
-    return HttpResponse('login')
+    login_form = AuthenticationForm(request.POST or None)
+    context = {}
+    context['form'] = login_form
+    if request.POST:
+        if login_form.is_valid():
+            username = login_form.cleaned_data.get('username')
+            email = login_form.cleaned_data.get('email')
+            password= login_form.cleaned_data.get('password')
+            
+            user = authenticate(request, username=username, email=email, password=password)
+            
+            if user is not None:
+                login(request, user=user)
+                login_form = AuthenticationForm(request.POST or None)
+                context['form'] = login_form
+                return redirect('recognizer:home')
+            else:
+                messages.error(request, 'User not found signup first!')
+                return render(request, 'recognizer/login.html', context=context)
+            
+    context = {}
+    return render(request, 'recognizer/login.html', context=context)
 
 
 def signup_view(request):
+    signup_form = AuthenticationForm(request.POST or None)
+    context = {
+        
+    }
+    context['form'] = signup_form
+    
+    if request.POST:
+        if signup_form.is_valid():
+            username = signup_form.cleaned_data.get('username')
+            email = signup_form.cleaned_data.get('email')
+            password = signup_form.cleaned_data.get('password')
+            
+            user = authenticate(request, username=username, email=email, password=password)
+            if user is None:
+                user = User.objects.create(username=username, email=email)
+                user.set_password(password)
+                user.save()
+                signup_form = AuthenticationForm(request.POST or None)
+                context['form'] = signup_form
+                messages.success(request, "Sign up Sucsessful")
+                return redirect('recognizer:home')
+            else:
+                messages.error(request, 'User already exists!')
+                context['form'] = signup_form
+                return render(request, 'recognizer/signup.html', context=context)
+                
+
+                
     return HttpResponse('sign up')
 
 
